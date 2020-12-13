a818830ba80e8ed9bfdd4ecac67f410f681a6134
Stu Lipshires <stu_lipshires@us.ibm.com>
2020-12-13 16:27:57 -0500

    sss

--- variables.tf
+++ variables.tf
@@ -27,36 +27,6 @@ variable "vcd_catalog" {
   description = "This is the vcd catalog to use for the environment."
 }
 
-variable "vsphere_server" {
-  type        = string
-  description = "This is the vSphere server for the environment."
-}
-
-variable "vsphere_user" {
-  type        = string
-  description = "vSphere server user for the environment."
-}
-
-variable "vsphere_password" {
-  type        = string
-  description = "vSphere server password"
-}
-
-variable "vsphere_cluster" {
-  type        = string
-  description = "This is the name of the vSphere cluster."
-}
-
-variable "vsphere_datacenter" {
-  type        = string
-  description = "This is the name of the vSphere data center."
-}
-
-variable "vsphere_datastore" {
-  type        = string
-  description = "This is the name of the vSphere data store."
-}
-
 variable "vm_template" {
   type        = string
   description = "This is the name of the VM template to clone."
