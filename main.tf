0c957bfe39e554a66a6fd3316736a1b47b61e485
borazem <32193129+borazem@users.noreply.github.com>
2022-09-26 20:19:26 +0200

    c

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ locals {
   control_plane_fqdns = [for idx in range(var.control_plane_count) : "control-plane-${idx}.${local.cluster_domain}"]
   compute_fqdns       = [for idx in range(var.compute_count) : "compute-${idx}.${local.cluster_domain}"]
   storage_fqdns       = [for idx in range(var.storage_count) : "storage-${idx}.${local.cluster_domain}"]
-  ssh_public_key      = var.ssh_public_key == "" ? chomp(tls_private_key.installkey[0].public_key_openssh) : chomp(file(pathexpand(var.ssh_public_key)))
+  # ssh_public_key      = var.ssh_public_key == "" ? chomp(tls_private_key.installkey[0].public_key_openssh) : chomp(file(pathexpand(var.ssh_public_key)))
+  ssh_public_key      = var.ssh_public_key
   folder_path         = var.vsphere_folder == "" ? var.cluster_id : var.vsphere_folder
   resource_pool_id    = var.vsphere_preexisting_resourcepool ? data.vsphere_resource_pool.resource_pool[0].id : vsphere_resource_pool.resource_pool[0].id
 }
